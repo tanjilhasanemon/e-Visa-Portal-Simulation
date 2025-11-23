ae7daa1e767e7679545f7f691a3e8e8a97680dd8
Tanjil Hasan Emon <tanjilsarkeremon@gmail.com>
2025-11-24 05:35:25 +0600

    modified scene swithc

--- e-Visa Portal Simulation/src/main/java/com/project/visa_management_portal/SceneSwitch.java
+++ e-Visa Portal Simulation/src/main/java/com/project/visa_management_portal/SceneSwitch.java
@@ -1,23 +1,22 @@
 package com.project.visa_management_portal;
 
+
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class SceneSwitch {
+    public static void switchTo(String fxml, ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource(fxml));
+        Scene scene = new Scene(fxmlLoader.load());
 
-    public static void SceneSwitcher(Node sourceNode,String userName, String fxmlFileName, String title) throws IOException {
-            FXMLLoader loader = new FXMLLoader(com.project.visa_management_portal.SceneSwitch.class.getResource("/dsit/pmfinal/meddy/"+ userName +"/"+ fxmlFileName));
-            Parent root = loader.load();
-
-            Stage stage = (Stage) sourceNode.getScene().getWindow();
-            stage.setTitle(title);
-            stage.setScene(new Scene(root));
-            stage.show();
-        }
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
     }
-
+}
